f34c4ee0b3b3c279be0341bef6c9086aa8614d65
vsyang <yang.vanessa85@gmail.com>
2024-02-08 22:58:19 -0700

    work on larger css tomorrow

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,10 +1,63 @@
-const directoryList = document.querySelector('.member-directory');
-const website = document.querySelector('#website');
-const imgBase = 'https://vsyang.github.io/wdd230/chamber/images/';
-const membersURL = 'https://vsyang.github.io/wdd230/chamber/data/members.json';
+const directoryList = document.querySelector('#member-directory');
+const gridButton = document.querySelector('#grid-button');
+const listButton = document.querySelector('#list-button'); 
+const imgBase = 'images/';
+const membersURL = 'data/members.json';
 
 async function getMembers() {
     const response = await fetch(membersURL);
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
+    displayMembers(data.members)
 }
+
+function displayMembers(data) {
+    directoryList.innerHTML = "";
+    if (gridButton.classList.contains('active')) {
+        directoryList.classList.remove('list');
+        directoryList.classList.add('grid');
+
+        data.forEach((member) => {
+            let cards = document.createElement('div');
+            let companyCard = document.createElement('img');
+            companyCard.setAttribute('src', imgBase + member.image);
+            companyCard.setAttribute('alt', `Business card for ${member.name}`);
+            companyCard.setAttribute('width', 300);
+            companyCard.setAttribute('height', 171);
+            companyCard.setAttribute('loading', 'lazy');
+            cards.appendChild(companyCard);
+            directoryList.appendChild(cards);
+        });
+    } else {
+        directoryList.classList.add('list');
+        directoryList.classList.remove('grid');
+
+        data.forEach((member) => {
+            const company = document.createElement('section');
+            company.classList.add('information');
+            company.innerHTML = `
+            <h3>${member.name}</h3>
+            <p>${member.address}</p>
+            <a href=https://${member.website}>${member.website}</a>
+            <p>${member.phone}</p>
+            <p>${member.businessType}</p>
+            <p>${member.memberLevel}</p>
+            `;
+            directoryList.appendChild(company);
+        });
+    }
+}
+
+gridButton.addEventListener('click', ()=> {
+    listButton.classList.remove('active');
+    gridButton.classList.add('active');
+    getMembers();
+});
+
+listButton.addEventListener('click', ()=> {
+    listButton.classList.add('active');
+    gridButton.classList.remove('active');
+    getMembers();
+});
+
+getMembers();
--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -4,7 +4,6 @@ const cards = document.querySelector('#cards');
 async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
-    // console.table(data.prophets);
     displayProphets(data.prophets);
 }
 
